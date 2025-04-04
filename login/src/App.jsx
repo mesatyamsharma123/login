aa58ea058a37f442d6497f05691e70c88d23c181
mesatyamsharma123 <96259558+mesatyamsharma123@users.noreply.github.com>
2025-04-04 22:52:31 +0530

    done

--- login/src/App.jsx
+++ login/src/App.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState,useEffect } from 'react'
+
 import './App.css'
 import Login from './component/login/Login'
 import Header from './component/header/Header'
-import useDark from '../src/component/dark/useDark'
 
 function App() {
- const {dark,setDark}=useDark();
+  const [dark,setDark]=useState(false)
+   useEffect(()=>{
+    if(dark){
+      document.documentElement.classList.add("dark")
+    }
+    else{
+      document.documentElement.classList.remove("dark")
+    }
+  
+  
+  
+   })
+ 
   return (
     <div className='flex flex-col min-h-screen  dark:bg-slate-700 items-center justify-center'>
     <Header dark={dark} setDark={setDark}/>
